5835d19c351a4896efaea9abadd14b7beaddbbb4
Simon Lim <150989115+SimonLim03@users.noreply.github.com>
2023-11-16 16:41:16 +1100

    Update api.py

--- api.py
+++ api.py
@@ -6,20 +6,8 @@ def call_get(url: str) -> requests.models.Response:
     Function that will call the API endpoint (input parameter) and return its response as a requests.models.Response object
     In case of an error, the program will exit and display the relevant message provided in the assignment brief
 
-    Parameters
-    ----------
-    # => URL: string
-
-    Pseudo-code
-    ----------
-    # => To be filled by student
-
-    Returns
-    -------
-    # => response object
     """
     
-    # => To be filled by student
 
     try:
         return requests.get(url=url)
